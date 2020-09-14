1fa8755b9317bd9c74002d04bd5ec0cb1ea165f2
Michael Naquin <mnaquin1@yahoo.com>
2020-09-14 18:14:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ function init() {
 
 let index = 0;
 
-  document.body.addEventListener("keydown", (function(e)  {
+document.body.addEventListener("keydown", (function(e)  {
 const key = e.key;
 
     if (codes[index] === key) {
